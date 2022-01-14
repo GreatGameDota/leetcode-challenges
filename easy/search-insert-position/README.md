328362e5d419de55399cac6dffb0a35059d0086e
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-01-14 14:31:55 -0600

    Add "Search Insert Position"

---
+++ easy/search-insert-position/README.md
@@ -0,0 +1,14 @@
+
+# Search Insert Position
+
+## Easy
+
+Date uploaded: 1/14/22
+
+Language: C++
+
+Runtime: 9 ms
+
+Memory Usage: 9.8 mb
+
+Details: The problem is simply searching for a target value in a sorted list and returning its index. If the number isn't in the list then return the index where it would be. My algorithm is just a binary search where I returned the final middle index if the number wasn't found. This algorithm runs in O(log(n)) runtime.
---
+++ easy/search-insert-position/solution.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int mid = nums.size() / 2;
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end)
+        {
+            if (nums[mid] < target)
+            {
+                start = mid + 1;
+                mid = (start + end) / 2;
+            }
+            else if (nums[mid] > target)
+            {
+                end = mid - 1;
+                mid = (start + end) / 2;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        if (nums[mid] > target)
+            return mid;
+        else
+            return mid + 1;
+    }
+};
