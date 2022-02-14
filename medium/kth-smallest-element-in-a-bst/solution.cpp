fde56ab979968d0494953856671dbe229c297e94
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-02-14 17:48:47 -0600

    Add "Kth Smallest Element in a BST"

---
+++ medium/kth-smallest-element-in-a-bst/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ret;
+    
+    int kthSmallest(TreeNode* root, int k) {
+        int num = 0;
+        dfs(root, k, num);
+        return ret;
+    }
+    
+    void dfs(TreeNode *n, int k, int &num) {
+        if (!n)
+            return;
+        
+        dfs(n->left, k, num);
+        num++;
+        
+        if (num == k)
+            ret = n->val;
+        
+        dfs(n->right, k, num);
+    }
+};
